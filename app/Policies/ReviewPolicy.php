b389061eadc433a2f1652a7e3ae353bbc06c9d31
yukit4mu <140926731+yukit4mu@users.noreply.github.com>
2025-12-30 11:32:49 -0500

    Fix: ReviewPolicyのupdate/deleteメソッドが値を返すように修正

--- app/Policies/ReviewPolicy.php
+++ app/Policies/ReviewPolicy.php
@@ -4,40 +4,15 @@ namespace App\Policies;
 
 use App\Models\Review;
 use App\Models\User;
-use Illuminate\Auth\Access\Response;
 
 class ReviewPolicy
 {
-    /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(User $user): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, Review $review): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        //
-    }
-
     /**
      * Determine whether the user can update the model.
      */
     public function update(User $user, Review $review): bool
     {
-        //
+        return $user->id === $review->user_id;
     }
 
     /**
@@ -45,22 +20,6 @@ class ReviewPolicy
      */
     public function delete(User $user, Review $review): bool
     {
-        //
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Review $review): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Review $review): bool
-    {
-        //
+        return $user->id === $review->user_id;
     }
 }
